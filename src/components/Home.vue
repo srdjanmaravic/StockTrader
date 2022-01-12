3440012a6a2c8da9061d1c329b512d966d7b6ff8
Maravic <srdjan.maravic@igt.com>
2022-01-12 12:49:09 +0100

    'initial'

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Trade or View your Portfolio</h1>
+    <h6>You may Load & Save your Data</h6>
+    <h6>Click on 'End Day' to begin New Day</h6>
+    <p>Your funds: {{funds | currency}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed:{
+    funds() {
+      return this.$store.getters.funds
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"></Stock>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import Stock from './Stock.vue'
+export default {
+  computed:{
+    ...mapGetters({
+        stocks: 'stocksPortfolio'
+    })
+  },
+  components: {
+    Stock
+  }
+}
+</script>
+
+<style>
+
+</style>
